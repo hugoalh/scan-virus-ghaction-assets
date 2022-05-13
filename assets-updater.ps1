1c6d318f7774ae61eb896dbc43c9763c6c2c56b9
hugoalh <32359235+hugoalh@users.noreply.github.com>
2022-05-13 16:44:59 +0800

    20220513A

--- assets-updater.ps1
+++ assets-updater.ps1
@@ -5,11 +5,11 @@
 Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath 'get-csv.psm1') -Scope 'Local'
 foreach ($Asset in $AssetsAvailable) {
 	[string]$AssetRoot = Join-Path -Path $PSScriptRoot -ChildPath $Asset
-	[pscustomobject[]]$AssetIndex = Get-Csv -Path (Join-Path -Path $AssetRoot -ChildPath 'index.tsv') -Delimiter "`t"
+	[pscustomobject[]]$AssetIndex = Get-Csv -LiteralPath (Join-Path -Path $AssetRoot -ChildPath 'index.tsv') -Delimiter "`t"
 	foreach ($Item in $AssetIndex) {
 		[string]$OutFileFullName = Join-Path -Path $AssetRoot -ChildPath $Item.Location
 		[string]$OutFileRoot = Split-Path -Path $OutFileFullName -Parent
-		if ((Test-Path -Path $OutFileRoot) -eq $false) {
+		if ((Test-Path -LiteralPath $OutFileRoot -PathType 'Container') -eq $false) {
 			New-Item -Path $OutFileRoot -ItemType 'Directory'
 		}
 		try {
@@ -20,3 +20,9 @@ foreach ($Asset in $AssetsAvailable) {
 		Start-Sleep -Seconds 2
 	}
 }
+[string]$Timestamp = Get-Date -UFormat '%Y-%m-%dT%H:%M:%SZ' -AsUTC
+[string]$AssetsMetaDataFullName = Join-Path -Path $PSScriptRoot -ChildPath 'assets-metadata.json'
+[hashtable]$AssetsMetaData = (Get-Content -LiteralPath $AssetsMetaDataFullName -Raw -Encoding 'UTF8NoBOM' | ConvertFrom-Json -AsHashtable -Depth 100)
+$AssetsMetaData.timestamp = $Timestamp
+Set-Content -LiteralPath $AssetsMetaDataFullName -Value (ConvertTo-Json -InputObject $AssetsMetaData -Depth 100 -Compress) -Confirm:$false -NoNewline -Encoding 'UTF8NoBOM'
+Write-Host -Object "::set-output name=timestamp::$Timestamp"
