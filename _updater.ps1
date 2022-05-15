3a21927e93ce96700fce418d03268e6682d622b5
hugoalh <32359235+hugoalh@users.noreply.github.com>
2022-05-15 17:26:36 +0800

    Add YARA rules

--- _updater.ps1
+++ _updater.ps1
@@ -7,6 +7,7 @@ Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '_get-csv.psm1') -
 	'clamav-unofficial-signatures',
 	'yara-rules'
 ) | ForEach-Object -Process {
+	Write-Progress -Activity $_ -Id 0
 	[string]$AssetRoot = Join-Path -Path $PSScriptRoot -ChildPath $_
 	[pscustomobject[]]$AssetIndex = Get-Csv -LiteralPath (Join-Path -Path $AssetRoot -ChildPath 'index.tsv') -Delimiter "`t"
 	$AssetIndex | Where-Object -FilterScript {
@@ -18,22 +19,25 @@ Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '_get-csv.psm1') -
 		}) -Depth 100 -Compress
 	} | Select-Object -Unique | ForEach-Object -Process {
 		[hashtable]$Item = ConvertFrom-Json -InputObject $_ -AsHashtable -Depth 100
+		Write-Progress -Activity $Item.Source -Id 1 -ParentId 0
 		[string]$GitWorkingDirectory = Join-Path -Path $AssetRoot -ChildPath $Item.Location
 		if (Test-Path -LiteralPath $GitWorkingDirectory) {
 			Remove-Item -LiteralPath $GitWorkingDirectory -Recurse -Force -Confirm:$false
 		}
 		Set-Location -LiteralPath $AssetRoot
 		try {
-			Invoke-Expression -Command "git --no-pager clone --no-tags --quiet --recurse-submodules `"$($Item.Source)`" `"$($Item.Location)`""
+			Invoke-Expression -Command "git --no-pager clone --quiet --recurse-submodules `"$($Item.Source)`" `"$($Item.Location)`""
 			Remove-Item -LiteralPath (Join-Path -Path $GitWorkingDirectory -ChildPath '.git') -Recurse -Force -Confirm:$false
 		} catch {
 			Write-Warning -Message $_
 		}
 		Set-Location -LiteralPath $PSScriptRoot
+		Write-Progress -Activity $Item.Source -Id 1 -ParentId 0 -Completed
 	}
 	$AssetIndex | Where-Object -FilterScript {
 		return (($_.UpdateMethod -eq 'WebRequest') -and ($TriggeredBy -match $_.UpdateCondition))
 	} | ForEach-Object -Process {
+		Write-Progress -Activity $_.Source -Id 2 -ParentId 0
 		[string]$OutFileFullName = Join-Path -Path $AssetRoot -ChildPath $_.Location
 		[string]$OutFileRoot = Split-Path -Path $OutFileFullName -Parent
 		if ((Test-Path -LiteralPath $OutFileRoot -PathType 'Container') -eq $false) {
@@ -46,7 +50,9 @@ Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '_get-csv.psm1') -
 			Write-Warning -Message $_
 		}
 		Start-Sleep -Seconds 1
+		Write-Progress -Activity $_ -Id 2 -ParentId 0 -Completed
 	}
+	Write-Progress -Activity $_ -Id 0 -Completed
 }
 [string]$Timestamp = Get-Date -UFormat '%Y-%m-%dT%H:%M:%SZ' -AsUTC
 Set-Content -LiteralPath (Join-Path -Path $PSScriptRoot -ChildPath '_timestamp.txt') -Value $Timestamp -Confirm:$false -NoNewline -Encoding 'UTF8NoBOM'
