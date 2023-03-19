f4997c83944c80444bbe89a6839fa07c941c6cca
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-03-19 14:24:18 +0800

    20230319A

--- _updater.ps1
+++ _updater.ps1
@@ -1,23 +1,26 @@
 #Requires -PSEdition Core
 #Requires -Version 7.2
-Write-Host -Object 'Begin process.'
 $Script:ErrorActionPreference = 'Stop'
-[String[]]$GitIgnore = Get-Content -LiteralPath (Join-Path -Path $PSScriptRoot -ChildPath '_updater_gitignore.txt') -Encoding 'UTF8NoBOM'
-[Hashtable]$TsvParameters = @{
+Import-Module -Name 'hugoalh.GitHubActionsToolkit' -Scope 'Local'
+Write-Host -Object 'Initialize.'
+[String[]]$GitIgnores = Get-Content -LiteralPath (Join-Path -Path $PSScriptRoot -ChildPath '_updater_gitignore.txt') -Encoding 'UTF8NoBOM' |
+	Where-Object -FilterScript { $_.Length -igt 0 }
+[Hashtable]$ImportCsvParameters_Tsv = @{
 	Delimiter = "`t"
 	Encoding = 'UTF8NoBOM'
 }
+[String]$MetadataFilePath = Join-Path -Path $PSScriptRoot -ChildPath 'metadata.json'
 [DateTime]$TimeInvoke = Get-Date -AsUTC
 [DateTime]$TimeBuffer = (Get-Date -Date $TimeInvoke -AsUTC).AddMinutes(-30)
 [String]$TimeCommit = Get-Date -Date $TimeInvoke -UFormat '%Y-%m-%dT%H:%M:%SZ' -AsUTC
-Write-Host -Object "$($PSStyle.Bold)Commit/Invoke Time:$($PSStyle.BoldOff) $TimeCommit"
+Write-Host -Object "$($PSStyle.Bold)Commit/Invoke Time: $($PSStyle.Reset)$TimeCommit"
+Write-Host -Object 'Config Git.'
+git --no-pager config 'user.name' 'github-actions'
+git --no-pager config 'user.email' 'github-actions@github.com'
 Write-Host -Object 'Import updater event trigger.'
 [AllowEmptyString()][String]$InputEvent = ($Env:GITHUB_EVENT_NAME ?? '') -ireplace '_', '-'
-If ($InputEvent -inotin @(
-	'schedule',
-	'workflow-dispatch'
-)) {
-	Write-Host -Object "::error::``$InputEvent`` is not a valid updater event trigger!"
+If ($InputEvent -inotin @('schedule', 'workflow-dispatch')) {
+	Write-GitHubActionsFail -Message "``$InputEvent`` is not a valid updater event trigger!"
 	Exit 1
 }
 [String[]]$ConditionsAvailable = @(
@@ -42,18 +45,17 @@ If ($InputSchedule.Length -igt 0) {
 }
 $ConditionsAvailable = $ConditionsAvailable |
 	Sort-Object -Unique
-Write-Host -Object "$($PSStyle.Bold)Conditions Available ($($ConditionsAvailable.Count)):$($PSStyle.BoldOff) $($ConditionsAvailable -join ', ')"
+Write-Host -Object "$($PSStyle.Bold)Conditions Available ($($ConditionsAvailable.Count)): $($PSStyle.BoldOff)$(
+	$ConditionsAvailable |
+		Join-String -Separator ', '
+)"
 Write-Host -Object 'Begin update assets.'
-ForEach ($AssetDirectory In @(
-	'clamav-signatures-ignore-presets',
-	'clamav-unofficial-signatures',
-	'yara-rules'
-)) {
+ForEach ($AssetDirectory In @('clamav-signatures-ignore-presets', 'clamav-unofficial-signatures', 'yara-rules')) {
 	Write-Host -Object "At ``$AssetDirectory``."
 	Write-Host -Object 'Read asset index.'
 	[String]$AssetRoot = Join-Path -Path $PSScriptRoot -ChildPath $AssetDirectory
 	[String]$AssetIndexFileFullPath = Join-Path -Path $AssetRoot -ChildPath 'index.tsv'
-	[PSCustomObject[]]$AssetIndex = Import-Csv -LiteralPath $AssetIndexFileFullPath @TsvParameters
+	[PSCustomObject[]]$AssetIndex = Import-Csv -LiteralPath $AssetIndexFileFullPath @ImportCsvParameters_Tsv
 	[String[]]$GitFinishSessions = @()
 	For ($AssetIndexNumber = 0; $AssetIndexNumber -ilt $AssetIndex.Count; $AssetIndexNumber++) {
 		[PSCustomObject]$AssetIndexItem = $AssetIndex[$AssetIndexNumber]
@@ -82,11 +84,11 @@ ForEach ($AssetDirectory In @(
 				Set-Location -LiteralPath $AssetRoot
 				Try {
 					Invoke-Expression -Command "git --no-pager clone --quiet --recurse-submodules `"$($AssetIndexItem.Source)`" `"$GitName`""
-					Get-ChildItem -LiteralPath $GitWorkingDirectoryRoot -Include $GitIgnore -Recurse -Force |
+					Get-ChildItem -LiteralPath $GitWorkingDirectoryRoot -Include $GitIgnores -Recurse -Force |
 						Remove-Item -Recurse -Force -Confirm:$False
 				}
 				Catch {
-					Write-Host -Object "::warning::$_"
+					Write-GitHubActionsWarning -Message $_
 				}
 				Set-Location -LiteralPath $PSScriptRoot
 				$GitFinishSessions += $GitSession
@@ -100,33 +102,32 @@ ForEach ($AssetDirectory In @(
 				New-Item -Path $OutFileRoot -ItemType 'Directory' -Confirm:$False |
 					Out-Null
 			}
-			Start-Sleep -Seconds 1
 			Try {
 				Invoke-WebRequest -Uri $AssetIndexItem.Source -UseBasicParsing -MaximumRedirection 1 -MaximumRetryCount 3 -RetryIntervalSec 10 -Method 'Get' -OutFile $OutFileFullName
 			}
 			Catch {
-				Write-Host -Object "::warning::$_"
+				Write-GitHubActionsWarning -Message $_
 			}
-			Start-Sleep -Seconds 1
 		}
 		Else {
-			Write-Host -Object "::warning::Cannot update ``$AssetDirectory/$($AssetIndexItem.Name)``, no available update method!"
+			Write-GitHubActionsWarning -Message "Unable to update ``$AssetDirectory/$($AssetIndexItem.Name)``, no available update method!"
 		}
 		$AssetIndex[$AssetIndexNumber].LastUpdateTime = $TimeCommit
 	}
 	Write-Host -Object "At ``$AssetDirectory``."
 	Write-Host -Object 'Update asset index.'
 	$AssetIndex |
-		Export-Csv -LiteralPath $AssetIndexFileFullPath @TsvParameters -NoTypeInformation -UseQuotes 'AsNeeded' -Confirm:$False
+		Export-Csv -LiteralPath $AssetIndexFileFullPath @ImportCsvParameters_Tsv -NoTypeInformation -UseQuotes 'AsNeeded' -Confirm:$False
 }
 Write-Host -Object 'Write metadata.'
-[String]$MetadataFullName = Join-Path -Path $PSScriptRoot -ChildPath 'metadata.json'
-[PSCustomObject]$Metadata = Get-Content -LiteralPath $MetadataFullName -Raw -Encoding 'UTF8NoBOM' |
+[PSCustomObject]$Metadata = Get-Content -LiteralPath $MetadataFilePath -Raw -Encoding 'UTF8NoBOM' |
 	ConvertFrom-Json -Depth 100
 $Metadata.Timestamp = $TimeCommit
-Set-Content -LiteralPath $MetadataFullName -Value (
+Set-Content -LiteralPath $MetadataFilePath -Value (
 	$Metadata |
 		ConvertTo-Json -Depth 100 -Compress
 ) -Confirm:$False -NoNewline -Encoding 'UTF8NoBOM'
-Add-Content -LiteralPath $Env:GITHUB_OUTPUT -Value "timestamp=$TimeCommit"
-Write-Host -Object 'End process.'
+Write-Host -Object 'Push Git commit.'
+git --no-pager add --all
+Invoke-Expression -Command "git --no-pager commit --message=`"Update assets on $TimeCommit`""
+git --no-pager push
