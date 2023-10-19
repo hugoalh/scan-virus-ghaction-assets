1f628732d454ec80645db76fb2c85f45595c3aaf
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-10-19 12:04:46 +0800

    20231019A

--- updater.ps1
+++ updater.ps1
@@ -2,25 +2,96 @@
 $Script:ErrorActionPreference = 'Stop'
 Import-Module -Name 'hugoalh.GitHubActionsToolkit' -Scope 'Local'
 Test-GitHubActionsEnvironment -Mandatory
-Enter-GitHubActionsLogGroup -Title 'Initialize.'
-$CurrentWorkingDirectory = Get-Location
+Write-Host -Object 'Initialize.'
+$CurrentWorkingDirectory = [System.Environment]::CurrentDirectory
+[String[]]$GitIgnores = @(
+	'.cfduplication.*',
+	'.dockerignore',
+	'.eslintrc.*',
+	'.git',
+	'.github',
+	'.markdownlint.*',
+	'.vscode',
+	'.vscodeignore',
+	'.yamllint.*',
+	'*.[0-9][0-9][0-9]',
+	'*.ai',
+	'*.alz',
+	'*.bat',
+	'*.bin',
+	'*.bmp',
+	'*.cab',
+	'*.cjs',
+	'*.cpuprofile',
+	'*.db',
+	'*.diff',
+	'*.dll',
+	'*.egg',
+	'*.eml',
+	'*.exe',
+	'*.gif',
+	'*.gz',
+	'*.gzip',
+	'*.html',
+	'*.iso',
+	'*.jpeg',
+	'*.jpg',
+	'*.js',
+	'*.json',
+	'*.lnk',
+	'*.log',
+	'*.mjs',
+	'*.pdf',
+	'*.pkg',
+	'*.png',
+	'*.ps',
+	'*.ps1',
+	'*.psd',
+	'*.psd1',
+	'*.psm1',
+	'*.py',
+	'*.rar',
+	'*.rb',
+	'*.sh',
+	'*.sql',
+	'*.tar',
+	'*.tar.gz',
+	'*.tar.gzip',
+	'*.tgz',
+	'*.tmp',
+	'*.ts',
+	'*.txt',
+	'*.xml',
+	'*.xps',
+	'*.yaml',
+	'*.yml',
+	'*.zip',
+	'Dockerfile',
+	'[Mm]akefile'
+)
 [Hashtable]$TsvParameters = @{
 	Delimiter = "`t"
 	Encoding = 'UTF8NoBOM'
 }
-[String[]]$AssetsDirectoryNames = @('clamav', 'yara')
-[String[]]$GitIgnores = Get-Content -LiteralPath (Join-Path -Path $PSScriptRoot -ChildPath '_updater_gitignore.txt') -Encoding 'UTF8NoBOM' |
-	Where-Object -FilterScript { $_.Length -gt 0 }
-[Boolean]$ShouldPush = $False
 [DateTime]$TimeInvoke = Get-Date -AsUTC
 [DateTime]$TimeBuffer = $TimeInvoke.AddHours(-1)
 [String]$TimeCommit = Get-Date -Date $TimeInvoke -UFormat '%Y-%m-%dT%H:%M:%SZ' -AsUTC
 Write-Host -Object "Timestamp: $TimeCommit"
-Exit-GitHubActionsLogGroup
+Set-GitHubActionsOutput -Name 'timestamp' -Value $TimeCommit
 Write-Host -Object 'Update assets.'
-ForEach ($AssetDirectoryName In $AssetsDirectoryNames) {
-	Write-Host -Object "Read ``$AssetDirectoryName`` asset index."
-	[String]$AssetDirectoryPath = Join-Path -Path $PSScriptRoot -ChildPath $AssetDirectoryName
+[PSCustomObject[]]$AssetsTypeMeta = @(
+	[PSCustomObject]@{
+		Name = 'ClamAV'
+		Path = 'clamav'
+	},
+	[PSCustomObject]@{
+		Name = 'YARA'
+		Path = 'yara'
+	}
+)
+ForEach ($AssetTypeMeta In $AssetsTypeMeta) {
+	Write-Host -Object "Read $($AssetTypeMeta.Name) asset index."
+	[String]$AssetDirectoryPath = Join-Path -Path $PSScriptRoot -ChildPath $AssetTypeMeta.Path
 	[String]$AssetIndexFilePath = Join-Path -Path $AssetDirectoryPath -ChildPath 'index.tsv'
 	[PSCustomObject[]]$AssetIndex = Import-Csv -LiteralPath $AssetIndexFilePath @TsvParameters
 	For ([UInt64]$AssetIndexRow = 0; $AssetIndexRow -lt $AssetIndex.Count; $AssetIndexRow += 1) {
@@ -28,30 +99,28 @@ ForEach ($AssetDirectoryName In $AssetsDirectoryNames) {
 		If ($AssetIndexItem.Group.Length -gt 0) {
 			Continue
 		}
-		Enter-GitHubActionsLogGroup -Title "At ``$AssetDirectoryName/$($AssetIndexItem.Name)``."
+		Enter-GitHubActionsLogGroup -Title "At ``$($AssetTypeMeta.Name)/$($AssetIndexItem.Name)``."
 		If ((Get-Date -Date $AssetIndexItem.Timestamp -AsUTC) -gt $TimeBuffer) {
 			Write-Host -Object 'No need to update.'
 			Exit-GitHubActionsLogGroup
 			Continue
 		}
-		Write-Host -Object 'Need to update.'
-		If ($AssetIndexItem.Remote -imatch '^https:\/\/github\.com\/[\da-z_.-]+\/[\da-z_.-]+\.git$') {
+		If ($AssetIndexItem.Remote -imatch '^https?:\/\/.+?\.git$') {
+			Write-Host -Object "Update via clone Git repository ``$($AssetIndexItem.Remote)``."
 			[String]$GitWorkingDirectoryName = $AssetIndexItem.Path -isplit '[\\\/]' |
 				Select-Object -Index 0
 			[String]$GitWorkingDirectoryPath = Join-Path -Path $AssetDirectoryPath -ChildPath $GitWorkingDirectoryName
 			If (Test-Path -LiteralPath $GitWorkingDirectoryPath) {
-				Write-Host -Object "Remove old elements."
 				Remove-Item -LiteralPath $GitWorkingDirectoryPath -Recurse -Force -Confirm:$False
 			}
-			Write-Host -Object "Update via clone Git repository ``$($AssetIndexItem.Remote)``."
 			Set-Location -LiteralPath $AssetDirectoryPath
 			Try {
-				git --no-pager clone $AssetIndexItem.Remote $GitWorkingDirectoryName
+				git --no-pager clone --depth 1 $AssetIndexItem.Remote $GitWorkingDirectoryName
 			}
 			Catch {
 				Write-GitHubActionsWarning -Message $_
 			}
-			Set-Location -LiteralPath $CurrentWorkingDirectory.Path
+			Set-Location -LiteralPath $CurrentWorkingDirectory
 			Get-ChildItem -LiteralPath $GitWorkingDirectoryPath -Include $GitIgnores -Recurse -Force |
 				Remove-Item -Recurse -Force -Confirm:$False -ErrorAction 'Continue'
 		}
@@ -70,7 +139,6 @@ ForEach ($AssetDirectoryName In $AssetsDirectoryNames) {
 			}
 		}
 		$AssetIndex[$AssetIndexRow].Timestamp = $TimeCommit
-		$ShouldPush = $True
 		Exit-GitHubActionsLogGroup
 	}
 	Write-Host -Object "Update ``$AssetDirectoryName`` asset index."
@@ -132,6 +200,3 @@ $(
 )
 "@
 }
-Write-Host -Object 'Conclusion.'
-Set-GitHubActionsOutput -Name 'should_push' -Value $ShouldPush.ToString().ToLower()
-Set-GitHubActionsOutput -Name 'timestamp' -Value $TimeCommit
